73779a3fa63962b5d4ab13e7d68f0d5e4aa808c3
define) <120991965+definem@users.noreply.github.com>
2023-06-02 22:44:31 +0500

    ex. 1 and 2 done

---
+++ index.js
@@ -0,0 +1,26 @@
+// ---- Exercise-2 and 3----
+
+// 1-method
+
+var a = 2
+
+if (a % 2 == 0) {
+    console.log('A is an even number');
+}else if (a % 2 == 1) {
+    console.log('A is not an even number');
+}else{
+    console.log('Not off topic');
+}
+
+// 2-method
+
+var num = +prompt('Enter a number')
+
+if (num % 2 == 0) {
+    console.log("A is an even number");
+}else if (num % 2 == 1) {
+    console.log('A is not an even number');
+}else{
+    console.log('Not off topic');
+}
+
